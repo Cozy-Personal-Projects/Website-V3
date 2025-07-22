937f9f3e1e26cb4c03f3d56b948473a76fff0514
ClaraCrazy <clara@crazyco.xyz>
2025-07-22 17:47:51 +0200

    Fix CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./style.css"
+import "styles/style.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { GridBackground } from "@/components/grid-background"
 
